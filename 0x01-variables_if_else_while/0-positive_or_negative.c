d116ba2f62e66d5d2e94af185891f98eeef72c30
Daodu Victor Olaoluwa <41231998+daoson12@users.noreply.github.com>
2022-06-04 02:23:05 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - entry point
- *
- * Return:always return 0
- *
- */
+* main - entry point
+*
+* Return:always return 0
+*
+*/
 
 int main(void)
 {
